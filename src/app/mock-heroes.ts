33d11182f915205f11f39de4a55ab13a1d76ecb6
Iyaam <niceinicein@gmail.com>
2018-03-31 19:04:16 +0500

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+//images used in app*/
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', img: '/assets/images/mr.jpg'},
+  { id: 12, name: 'Narco' , img: '/assets/images/narco.jpg'},
+  { id: 13, name: 'Bombasto' , img: '/assets/images/bombasta.jpg'},
+  { id: 14, name: 'Celeritas' , img: '/assets/images/celeritas.jpg'},
+  { id: 15, name: 'Magneta' , img: '/assets/images/magn.jpg'},
+  { id: 16, name: 'RubberMan' , img: '/assets/images/rubber.jpg'},
+  { id: 17, name: 'Dynama' , img: '/assets/images/dynma.jpg'},
+  { id: 18, name: 'Dr IQ' , img: '/assets/images/index.jpg'},
+  { id: 19, name: 'Magma' , img: '/assets/images/magma.jpg'},
+  { id: 20, name: 'Tornado' , img: '/assets/images/tornado.jpg'}
+];
